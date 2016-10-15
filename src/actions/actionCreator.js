77142be25a50227dee334bdb5a12afdf4d9dffa0
Simon Nonnis <simononnis@gmail.com>
2016-10-15 12:51:51 +0100

    'Add actionCreators'

---
+++ src/actions/actionCreator.js
@@ -0,0 +1,37 @@
+// action
+const ADD_TODO = 'ADD_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+// helper
+const uID = Math.random().toString(32).slice(2);
+
+// action creator
+export function addToDo (text) {
+  return {
+    type: ADD_TODO,
+    payload: {
+      id: uID(),
+      isDone: false,
+      text: text
+    }
+  }
+}
+
+export function toggleToDo(todoId) {
+  return {
+    type: TOGGLE_TODO,
+    payload: {
+      id: todoId
+    }
+  }
+}
+
+export function deleteToDo(todoId) {
+  return {
+    type: DELETE_TODO,
+    payload: {
+      id: todoId
+    }
+  }
+}
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,46 @@
+const initialState = {
+  data: [
+    { id: '0', isDone: true,  text: 'Learn JavaScript' },
+    { id: '1', isDone: false, text: 'Learn React' },
+    { id: '2', isDone: false, text: 'Learn Redux' },
+    { id: '3', isDone: false, text: 'Learn ImmutableJs' }
+  ]
+};
+
+export const todoApp = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return Object.assign({}, state, {
+        data: [
+          ...state.data,
+          action.payload
+        ]
+      })
+    case 'TOGGLE_TODO':
+      return Object.assign({}, state, {
+        data: state.data.map(todo => {
+          if (action.payload.id === todo.id) {
+            return Object.assign({}, todo, {
+              isDone: !todo.isDone
+            })
+          } else {
+            return todo
+          }
+        })
+      })
+    case 'DELETE_TODO':
+      return Object.assign({}, state, {
+        data: state.data.map(todo => {
+          if (action.payload.id === todo.id) {
+            return Object.assign({}, todo, {
+              todo: null
+            })
+          } else {
+            return todo
+          }
+        })
+      })
+    default:
+      return state
+  }
+}
